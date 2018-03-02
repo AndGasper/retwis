e54a0e865d99250292aebb7dbdcf9b8383b79c8f
AndGasper <24193043+AndGasper@users.noreply.github.com>
2018-03-01 17:44:50 -0800

    Initial commit - Another crash course fumbling through webpack. Hot module reload partially works.

---
+++ retwis-react/client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BasicExample from './components/app';
+import Simple from './components/simple';
+ReactDOM.render(
+    <Simple />,
+    document.getElementById('root')
+);
